d346e505978c3807bbdbc2a9541405068a2226be
a20140114 <131030553+a20140114@users.noreply.github.com>
2023-09-25 13:29:55 -0500

    Add files via upload

---
+++ condicionales.js
@@ -0,0 +1,8 @@
+num = parseInt(prompt("Ingrese un número: "));
+if(num>0){
+    document.write("El número es positivo");
+}else if(num<0){
+    document.write("El número es negativo")
+}else{
+    document.write("El número es neutro")
+}
